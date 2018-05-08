ced8c67ffa681e0f7f61c319c409b6daf93339ff
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-08 15:43:35 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,46 +1,37 @@
-def loop_iterator(number_of_times)
-  thank_you = 0
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
+   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+
   loop do
     puts phrase
-    thank_you += 1
-    break if phrase == number_of_times
-end
-end
-
-def times_iterator(number_of_times)
-  number_of_times.times do
-  puts "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+    break if counter == number_of_times
   end
-end
-
-def while_iterator(number_of_times)
-  thank_you = 0
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  while thank_you < number_of_times
-  thank_you += 1
+ end
+ 
+ def times_iterator(number_of_times)
+  number_of_times.times do
   puts phrase
-end
-end
+  end
+ end
+ 
+ def while_iterator(number_of_times)
+   while counter < number_of_times do
+     puts phrase
+     counter += 1
+   end
+ end
+ 
+ def until_iterator(number_of_times)
+  until counter == number_of_times
+      puts phrase
+      counter += 1
+  end
+ end
+ 
 
-def until_iterator(number_of_times)
-  thank_you = 0
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-  until thank_you == number_of_times
-  puts phrase
-end
-
-end
-
-def for_iterator(number_of_times)
-  thank_you = 1..number_of_times
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  for thanks in number_of_times do
+ def for_iterator(number_of_times)
+  for item in 1..number_of_times do
     puts phrase
   end
-  
-end
-
+ end
